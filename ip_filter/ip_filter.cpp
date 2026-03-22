0ceefdf1cb1ecbec7e1711baa5d254f681953d33
yakovleva-ns <msnatasha@mail.ru>
2026-03-22 20:17:51 +0300

    Update ip_filter.cpp

--- ip_filter/ip_filter.cpp
+++ ip_filter/ip_filter.cpp
@@ -1,192 +1,76 @@
-#include <cassert>
-#include <cstdlib>
-#include <iostream>
 #include <algorithm>
+#include <cstdlib>
 #include <string>
 #include <vector>
+#include "ip_filter.h"
 
-// ("",  '.') -> [""]
-// ("11", '.') -> ["11"]
-// ("..", '.') -> ["", "", ""]
-// ("11.", '.') -> ["11", ""]
-// (".11", '.') -> ["", "11"]
-// ("11.22", '.') -> ["11", "22"]
 
-typedef std::vector<std::string> Ip;
+IpFilter::IpFilter()
+{};
 
-Ip split(const std::string &str, char d)
+bool needSwapIp(Ip &ip1, Ip &ip2)
 {
-    Ip r;
+    if (stoi(ip1[0]) > stoi(ip2[0]))
+            return true;
 
-    std::string::size_type start = 0;
-    auto stop = str.find_first_of(d);
-    while(stop != std::string::npos)
-    {
-        r.push_back(str.substr(start, stop - start));
+    if (stoi(ip1[0]) == stoi(ip2[0]))
+        if (stoi(ip1[1]) > stoi(ip2[1]))
+            return true;
 
-        start = stop + 1;
-        stop = str.find_first_of(d, start);
-    }
+    if (stoi(ip1[0]) == stoi(ip2[0]))
+    if (stoi(ip1[1]) == stoi(ip2[1]))
+        if (stoi(ip1[2]) > stoi(ip2[2]))
+            return true;
 
-    r.push_back(str.substr(start));
+    if (stoi(ip1[0]) == stoi(ip2[0]))
+    if (stoi(ip1[1]) == stoi(ip2[1]))
+    if (stoi(ip1[2]) == stoi(ip2[2]))
+        if (stoi(ip1[3]) > stoi(ip2[3]))
+            return true;
 
-    return r;
-}
-
-bool needSwapIp(Ip ip1, Ip ip2)
-{
-    if ( (ip1.size() == 4) && (ip2.size() == 4) )
-    {
-        for (int i = 0 ; i <= 4 ; i++)
-        {
-            if (stoi(ip1[i]) < stoi(ip2[i]))
-                return true;
-        }
-    }
     return false;
 }
 
-
-int partition(std::vector<Ip> ip_pool, int start, int end)
+std::vector<Ip> IpFilter::filterByte(std::vector<Ip> ipPool, int byte)
 {
-    Ip referenceIp = ip_pool[end];
-    int index = start;
+    std::vector<Ip> ipPoolSort;
 
-    for(int i = start; i < end; i++)
+    for(Ip ip : ipPool)
     {
-        if (needSwapIp(ip_pool[i], referenceIp))
-        {
-            ip_pool[i].swap(ip_pool[index]);
-            index ++;
-        }
+        if (stoi(ip[0]) == byte)
+            ipPoolSort.push_back(ip);
     }
 
-    (referenceIp).swap(ip_pool[index]);
-    return index;
+    return ipPoolSort;
 }
 
-void sortIpQuick(std::vector<Ip> ip_pool, int start, int end)
+std::vector<Ip> IpFilter::filterByte(std::vector<Ip> ipPool, int firstByte, int secondByte)
 {
-    if (start >= end)
-        return;
+    std::vector<Ip> ipPoolSort;
 
-    int pivot = partition(ip_pool, start, end);
+    for(Ip ip : ipPool)
+    {
+        if ((stoi(ip[0]) == 46) && (stoi(ip[1]) == 70))
+            ipPoolSort.push_back(ip);
+    }
 
-    sortIpQuick(ip_pool, start, pivot-1);
-    sortIpQuick(ip_pool, pivot+1, end);
+    return ipPoolSort;
 }
 
-void sortIpBubble(std::vector<Ip> ip_pool)
+std::vector<Ip> IpFilter::filterAnyByte(std::vector<Ip> ipPool, int byte)
 {
-    for(int i = 0; i != ip_pool.size(); ++i)
+    std::vector<Ip> ipPoolSort;
+
+    for(Ip ip : ipPool)
     {
-        for(auto ip = ip_pool.begin(); ip != ip_pool.end()-i; ++ip)
-        {
-            if (needSwapIp(*ip, *(ip+1)))
-                (*ip).swap(*(ip+1));
-        }
+        if ((stoi(ip[0]) == byte) || (stoi(ip[1]) == byte) || (stoi(ip[2]) == byte) || (stoi(ip[3]) == byte))
+            ipPoolSort.push_back(ip);
     }
+
+    return ipPoolSort;
 }
 
-int main(int argc, char const *argv[])
+void IpFilter::sortStd(std::vector<Ip> &ipPool)
 {
-    try
-    {
-        std::vector<Ip> ip_pool;
-
-        for(std::string line; std::getline(std::cin, line);)
-        {
-            auto v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
-        }
-
-        // reverse lexicographically sort
-        
-        //sortIpQuick(ip_pool, 0, ip_pool.size()-1);
-        //sortIpBubble(ip_pool);
-        std::sort(ip_pool.begin(), ip_pool.end(), needSwapIp);
-
-        for(auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-        {
-            for(auto ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-            {
-                if (ip_part != ip->cbegin())
-                {
-                    std::cout << ".";
-                }
-                std::cout << *ip_part;
-            }
-            std::cout << std::endl;
-        }
-
-
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first byte and output
-        // ip = filter(1)
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
-    }
-    catch(const std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-
-    return 0;
+    std::sort(ipPool.begin(), ipPool.end(), needSwapIp);
 }
