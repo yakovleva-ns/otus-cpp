941fa0386126b5cf867149d5daa51e71469b3a29
yakovleva-ns <msnatasha@mail.ru>
2026-03-23 23:19:56 +0300

    fixMain

--- 2_ip_filter/main.cpp
+++ 2_ip_filter/main.cpp
@@ -51,15 +51,24 @@ void printIpPool(std::vector<Ip> ipPool)
     }
 }
 
-int main(int /*argc*/, char** /*argv*/) {
+int main(int /*argc*/, char** /*argv*/)
 {
-    auto ipPool =  readIpPool();
+    try
+    {
+        auto ipPool =  readIpPool();
 
-    IpFilter filter;
-    filter.sortStd(ipPool);
+        IpFilter filter;
+        filter.sortStd(ipPool);
 
-    printIpPool(ipPool);
-    printIpPool(filter.filterByte(ipPool, 1));
-    printIpPool(filter.filterByte(ipPool, 46, 70));
-    printIpPool(filter.filterAnyByte(ipPool, 46));
+        printIpPool(ipPool);
+        printIpPool(filter.filterByte(ipPool, 1));
+        printIpPool(filter.filterByte(ipPool, 46, 70));
+        printIpPool(filter.filterAnyByte(ipPool, 46));
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
 }
