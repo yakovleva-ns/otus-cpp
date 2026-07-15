6d2a31f5247838bd7279a4e0440ac21af20f7c08
yakovleva-ns <msnatasha@mail.ru>
2026-07-15 16:42:01 +0300

    Update model.cpp

--- Editor/model.cpp
+++ Editor/model.cpp
@@ -1,23 +1,28 @@
-#include "model.h"
+#include "Model.h"
 #include <iostream>
+
 
   Model::Model()
   {
-
+      std::cout << "Model create!"<< std::endl;
   }
     
   Model::Model(std::string filename)
   {
-      std::cout << "Model create!"<<filename<< std::endl;
+      std::cout << "Model create!"<< std::endl;
+      loadFromFile(filename);
   }
-void Model::show() const {
-    for (const auto& [id, primitive] : m_mapPrimitives) {
+
+void Model::show() const 
+{
+    for (const auto& [id, primitive] : m_mapPrimitives) 
+    {
         primitive->draw();
     }
     std::cout << "Model show!"<< std::endl;
 }
 
-int Model::addPrimitive(PtrPrimitive pPrimitive) 
+int Model::addPrimitive(const PtrPrimitive &pPrimitive) 
 {
     int last = m_mapPrimitives.size();
     m_mapPrimitives[last] = pPrimitive;
@@ -26,15 +31,26 @@ int Model::addPrimitive(PtrPrimitive pPrimitive)
     return last;
 }
 
-
-void Model::removePrimitive(int id) {
-    auto it = m_mapPrimitives.find(id); 
-    if (it != m_mapPrimitives.end())
-        m_mapPrimitives.erase(it);
+void Model::removePrimitive(int id) 
+{
+    if (m_mapPrimitives.contains(id)) 
+        m_mapPrimitives.erase(id);
 }
 
-void Model::removePrimitive(PtrPrimitive pPrimitive) 
+void Model::removePrimitive(const PtrPrimitive &pPrimitive) 
 {
     int id = pPrimitive.get()->getId();
     removePrimitive(id);
+}
+
+bool Model::loadFromFile(const std::string &filename)
+{
+    std::cout << "Model loaded! \n";
+    return true; //or false
+}
+
+bool Model::saveToFile(const std::string &filename)
+{
+    std::cout << "Model export to file!"<< std::endl;
+    return true; //or false
 }
